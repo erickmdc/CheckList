dcf5f99661c6ac7e79a597aec38e7020deec4574
erickmdc <emc.7@hotmail.com>
2017-03-28 13:47:56 -0300

    Add files via upload

---
+++ CheckListDB.sql
@@ -0,0 +1,34 @@
+-- user: root	password: root;
+
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`task`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`task` (
+  `TaskId` INT(11) NOT NULL AUTO_INCREMENT,
+  `Description` VARCHAR(80) NOT NULL,
+  `Status` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`TaskId`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
